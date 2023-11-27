75e7f1984a9c91787b6790ca46e87123d9129e6c
AlineFernandaDev <alinefds.vieira@gmail.com>
2023-11-27 16:42:43 -0300

    Primeiro Commit

---
+++ Classificador-Nivel-Heroi.js
@@ -0,0 +1,24 @@
+const XP = 10500;
+let nickname = "Robertinho";
+let nivel = "";
+
+if(XP < 1000){
+    nivel = "Ferro";
+}else if(XP >= 1001 && XP <= 2000){
+    nivel = "Bronze";
+}else if(XP >=2001 && XP <= 5000){
+    nivel = "Prata";
+}else if(XP >= 6001 && XP<= 7000){
+    nivel = "Ouro";
+}else if(XP >= 7001 && XP <= 8000){
+    nivel = "Platina";
+}else if(XP >= 8001 && XP <= 9000){
+    nivel = "Ascendente";
+}else if(XP >= 9001 && XP <= 10000){
+    nivel = "Imortal";
+}else{
+    nivel = "Radiante";
+}
+console.log(`O Herói de nome ${nickname} está no nível de ${nivel}.`);
+
+
